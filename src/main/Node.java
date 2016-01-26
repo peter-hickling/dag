bcb96ad6da45381c3d31a78d874d81fba002df43
Peter Hickling <peter.hickling@codera.co.uk>
2016-01-26 12:48:07 +0000

    updated maven with test/main

--- src/main/Node.java
+++ src/main/Node.java
@@ -1,3 +1,5 @@
+package main;
+
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
--- src/test/NodeTest.java
+++ src/test/NodeTest.java
@@ -1,3 +1,5 @@
+package test;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -7,18 +9,21 @@ import java.util.Collection;
 
 import org.junit.Test;
 
+import main.Node;
+
 public class NodeTest {
 
 	@Test
 	public void testNodeHasName() {
 		Node<String> node = new Node<String>("Test");
-		
+
 		assertEquals("Test", node.getData());
 	}
+
 	@Test
 	public void testToString() {
 		Node<String> node = new Node<String>("Test");
-		
+
 		assertEquals("Test", node.toString());
 	}
 
@@ -26,11 +31,11 @@ public class NodeTest {
 	public void testAddChild() {
 		Node<String> parent = new Node<String>("Parent");
 		Node<String> child = new Node<String>("Child");
-		
+
 		assertFalse(parent.hasChild(child));
-		
+
 		parent.addChild(child);
-		
+
 		assertTrue(parent.hasChild(child));
 	}
 
@@ -38,22 +43,22 @@ public class NodeTest {
 	public void testAddSameChildTwice() {
 		Node<String> parent = new Node<String>("Parent");
 		Node<String> child = new Node<String>("Child");
-		
+
 		parent.addChild(child);
 		parent.addChild(child);
-		
+
 		assertEquals(1, parent.getChildren().size());
 	}
-	
+
 	@Test
 	public void testAddSameChildNameTwice() {
 		Node<String> parent = new Node<String>("Parent");
 		Node<String> child1 = new Node<String>("Child1");
 		Node<String> child2 = new Node<String>("Child1");
-		
+
 		parent.addChild(child1);
 		parent.addChild(child2);
-		
+
 		assertEquals(1, parent.getChildren().size());
 	}
 
@@ -73,45 +78,45 @@ public class NodeTest {
 	public void testReturnImmutableCollection() {
 		Node<String> drink = new Node<String>("Drink");
 		Node<String> alcoholic = new Node<String>("Alcoholic");
-		
+
 		drink.addChild(alcoholic);
 		Collection<Node<String>> children = drink.getChildren();
-		
+
 		assertEquals(1, drink.getChildren().size());
-		
+
 		try {
-			
+
 			children.add(new Node<String>("jobber"));
 			fail();
 		} catch (Exception e) {
 		}
 	}
-	
+
 	@Test
 	public void testCountDescendents() {
 		Node<String> parent = new Node<String>("Parent");
 		Node<String> child1 = new Node<String>("Child1");
 		Node<String> grandchild1 = new Node<String>("Grandchild1");
 		Node<String> greatgrandchild1 = new Node<String>("GreatGrandchild1");
-		
+
 		assertEquals(0, parent.getDescendentCount());
 
 		parent.addChild(child1);
 		child1.addChild(grandchild1);
-		
+
 		assertEquals(2, parent.getDescendentCount());
-		
+
 		Node<String> child2 = new Node<String>("Child2");
 		Node<String> grandchild2 = new Node<String>("Grandchild2");
-		
+
 		parent.addChild(child2);
 		child1.addChild(grandchild2);
 		grandchild1.addChild(greatgrandchild1);
-		
+
 		assertEquals(5, parent.getDescendentCount());
-		
+
 	}
-	
+
 	@Test
 	public void testCountComplexDescendents() {
 		Node<String> parent = new Node<String>("Parent");
@@ -121,7 +126,7 @@ public class NodeTest {
 		Node<String> grandchild2 = new Node<String>("Grandchild2");
 		Node<String> grandchild3 = new Node<String>("Grandchild3");
 		Node<String> greatgrandchild1 = new Node<String>("GreatGrandchild1");
-		
+
 		parent.addChild(child1);
 		parent.addChild(child2);
 		child1.addChild(grandchild1);
@@ -129,37 +134,36 @@ public class NodeTest {
 		child2.addChild(grandchild3);
 		grandchild1.addChild(greatgrandchild1);
 		grandchild2.addChild(greatgrandchild1);
-		
+
 		assertEquals(6, parent.getDescendentCount());
-		
+
 		assertTrue(parent.getDescendents().contains(greatgrandchild1));
 		assertTrue(parent.getDescendents().contains(grandchild1));
 		assertTrue(parent.getDescendents().contains(child1));
 		System.out.println(parent.getDescendents());
 	}
-	
+
 	@Test
 	public void testRandomData() {
 		Node<Integer> node = new Node<Integer>(7);
-		
+
 		assertEquals(Integer.valueOf(7), node.getData());
 	}
-	
+
 	@Test
 	public void testAcyclic() {
 		Node<String> parent = new Node<String>("Parent");
 		Node<String> child = new Node<String>("Child");
 		Node<String> grandChild = new Node<String>("Grandchild");
-		
+
 		parent.addChild(child);
 		child.addChild(parent);
 		child.addChild(grandChild);
 		grandChild.addChild(parent);
-		
+
 		assertFalse(child.getDescendents().contains(parent));
 		assertFalse(grandChild.getDescendents().contains(parent));
-		
-		
+
 	}
-	
+
 }
